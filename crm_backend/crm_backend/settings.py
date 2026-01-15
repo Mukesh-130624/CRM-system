e421d0473ea56696a696b425205c7bac8856e8e3
Mukesh-130624 <mukesh.saini130624@gmail.com>
2026-01-15 13:52:33 +0530

    Fix settings for production deployment: merge REST config, load .env, static files

--- crm_backend/crm_backend/settings.py
+++ crm_backend/crm_backend/settings.py
@@ -130,31 +130,21 @@ REST_FRAMEWORK = {
 
 INSTALLED_APPS += ["django_filters"]
 
-REST_FRAMEWORK["DEFAULT_FILTER_BACKENDS"] = [
-    "django_filters.rest_framework.DjangoFilterBackend",
-    "rest_framework.filters.SearchFilter",
-    "rest_framework.filters.OrderingFilter",
-]
-
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
     "EXCEPTION_HANDLER": "core.exceptions.custom_exception_handler",
-}
-
-# REST_FRAMEWORK = {
-#     "DEFAULT_AUTHENTICATION_CLASSES": [
-#         "rest_framework.authentication.SessionAuthentication",
-#     ],
-#     "DEFAULT_PERMISSION_CLASSES": [
-#         "rest_framework.permissions.IsAuthenticated",
-#     ],
-# }
-
-REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
